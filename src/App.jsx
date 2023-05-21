8f101a724b5c680227d4814297e1493bdcf26b7d
Salvador Espinosa <espinosasalvador94@outlook.com>
2023-05-21 16:58:47 -0600

    added dependencies and ready to start coding with Tailwind to create portfolio

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+
+  return (
+   <div className="w-screen h-screen bg-red-300">
+    
+   </div>
+  )
+}
+
+export default App
